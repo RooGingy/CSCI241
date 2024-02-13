2b21238ae1e3d41d0e0128f5305bbda50f792be1
RooGingy <71571395+RooGingy@users.noreply.github.com>
2024-02-13 12:07:59 -0600

    a

---
+++ Assign10Records/jhava.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class jhava here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class jhava
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    final int paint=250;
+    
+    public void main(String args){
+        System.out.print(paint);
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
